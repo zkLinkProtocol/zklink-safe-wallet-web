98ea399008559aff23bd750c59ff4505d9d7976b
nick8319 <zhiltsov.nick@gmail.com>
2024-03-27 20:11:13 +0100

    feat: update metatags

--- src/components/common/MetaTags/index.tsx
+++ src/components/common/MetaTags/index.tsx
@@ -3,8 +3,7 @@ import { ContentSecurityPolicy, StrictTransportSecurity } from '@/config/securit
 import lightPalette from '@/components/theme/lightPalette'
 import darkPalette from '@/components/theme/darkPalette'
 
-const descriptionText =
-  'Safe (prev. Gnosis Safe) is the most trusted platform to manage digital assets on Ethereum and multiple EVMs. Over $40B secured.'
+const descriptionText = 'zkLink Safe is the most trusted platform to manage digital assets on zkLink'
 const titleText = 'zkLink Safe'
 
 const MetaTags = ({ prefetchUrl }: { prefetchUrl: string }) => (
@@ -17,7 +16,7 @@ const MetaTags = ({ prefetchUrl }: { prefetchUrl: string }) => (
     <meta name="og:description" content={descriptionText} />
     <meta name="og:title" content={titleText} />
     <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:site" content="@safe" />
+    <meta name="twitter:site" content="@zkLink_Official" />
     <meta name="twitter:title" content={titleText} />
     <meta name="twitter:description" content={descriptionText} />
     <meta name="twitter:image" content="https://app.safe.global/images/social-share.png" />
